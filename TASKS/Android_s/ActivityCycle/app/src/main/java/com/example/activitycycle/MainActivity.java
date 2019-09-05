a9f84572e42ef8c24de6faf60237365045853ab9
Tasmia <tasmiabanu.kk@gmail.com>
2019-09-05 23:10:47 +0530

    Activity-android

--- TASKS/Android_s/ActivityCycle/app/src/main/java/com/example/activitycycle/MainActivity.java
+++ TASKS/Android_s/ActivityCycle/app/src/main/java/com/example/activitycycle/MainActivity.java
@@ -1,63 +1,41 @@
-package com.example.activitylifecycle;
+package com.example.activitycycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button Request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "First onCreate() calls", Toast.LENGTH_SHORT).show(); //onCreate called
         Log.d("lifecycle","onCreate invoked");
-        Request = findViewById(R.id.button);
-
-        Request.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), next_page.class);
-                startActivity(intent);
-            }
-        });
     }
-
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(getApplicationContext(), "This is onStart()", Toast.LENGTH_LONG).show(); //onStart Called
         Log.d("lifecycle","onStart invoked");
     }
-
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("lifecycle", "onResume invoked");
+        Log.d("lifecycle","onResume invoked");
     }
-
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("lifecycle", "onPause invoked");
+        Log.d("lifecycle","onPause invoked");
     }
-
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("lifecycle", "onStop invoked");
+        Log.d("lifecycle","onStop invoked");
     }
-
     @Override
     protected void onRestart() {
         super.onRestart();
-
         Log.d("lifecycle","onRestart invoked");
     }
     @Override
@@ -65,5 +43,4 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d("lifecycle","onDestroy invoked");
     }
-
 }
