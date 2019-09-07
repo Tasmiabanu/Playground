e3f6021b5dad684ea2b6606d2b8948c2913de038
Tasmia <tasmiabanu.kk@gmail.com>
2019-09-07 11:27:08 +0530

    TASK-class

--- TASKS/Android_s/Object_class/ObjectClass/app/src/main/java/com/example/objectclass/MainActivity.java
+++ TASKS/Android_s/Object_class/ObjectClass/app/src/main/java/com/example/objectclass/MainActivity.java
@@ -12,10 +12,10 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         object b=new object();
-         demo();
+         object();
     }
 
-    public void demo() {
+    public void object() {
         Toast.makeText(getApplicationContext(),"This is object", Toast.LENGTH_SHORT).show();
     }
 }
