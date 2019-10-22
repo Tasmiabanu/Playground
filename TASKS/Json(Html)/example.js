01266397e5b3d85ff85265f70753455a1f23a16e
Tasmia <tasmiabanu.kk@gmail.com>
2019-10-22 17:10:20 +0530

    Json data in Html

---
+++ TASKS/Json(Html)/example.js
@@ -0,0 +1,17 @@
+myFunction([
+{
+"display": "Name:TAS",
+
+},
+{
+"display": "Customer-No:9448900667",
+},
+{
+"display": "Customer Photo:",
+"logoLocation": "../img/Desktop/image.png"
+},
+{
+"display": "Legends Photo:",
+
+}
+])
